5e7d7c53d7d321cbd480e51802526d5badafe09a
Nirit Nagar <nirit.nn@gmail.com>
2022-08-03 20:02:36 +0200

    index to root and results comp

--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -6,12 +6,11 @@ import NextPrevButtons from "./NextPrevButtons";
 import SearchBar from './SearchBar';
 
 
-
 const apikey = process.env.REACT_APP_API_KEY;
 
 
 const getApiData = async (pageNum: number, searchQuery:string) => {
-        let rspns = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&fl=headline&fl=pub_date&fl=snippet&fl=web_url&facet_fields=document_type&page=${pageNum}&api-key=${apikey}`)
+        let rspns = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&fl=_id&fl=headline&fl=pub_date&fl=snippet&fl=web_url&facet_fields=document_type&page=${pageNum}&api-key=${apikey}`)
     return rspns.data.response.docs;
 }
 
@@ -20,7 +19,6 @@ export default function Results() {
     const [pageNum, setPageNum] = useState(1); 
     const [searchQuery, setSearchQuery] = useState("Tech");
     const { data, status } = useQuery(['results', pageNum, searchQuery], () => getApiData(pageNum, searchQuery), { keepPreviousData: true });
-
     return (
         <Container>
             <Logo><img src={logo} alt="logo" /></Logo>    
@@ -42,9 +40,9 @@ export default function Results() {
             <>
                 <ResultsContainer>
                     <ul>
-                        {data.map((items: any, index: number) => (
-                            <li key={index} onClick={() => window.location.href=`${items.web_url}`}>
-                                <p> {items.headline.main}</p>  <span>Read More ⇨</span>
+                        {data.map((item: any) => (
+                            <li key={item._id} onClick={() => window.location.href=`${item.web_url}`}>
+                                <p> {item.headline.main}</p>  <span>Read More ⇨</span>
                             </li>
                         ))}
                     </ul>
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,14 @@ import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
+const portServerNum = 8000;
+const express = require('express');
+const cors = require('cors');
 
+const appex = express();
+appex.listen(
+  8000, () => console.log(`Server running on port ${portServerNum}`)
+)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
